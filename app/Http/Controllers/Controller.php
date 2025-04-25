9d277d6fa9573436211f7432f11a9469a2b434f6
Eduardo Bacarin <eduardo.obacarin@gmail.com>
2025-04-24 21:40:34 -0300

    make default response functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,67 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function responseCreated($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Created sucessfully'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 201);
+    }
+
+    public function responseOk($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Queried sucessfully'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 200);
+    }
+
+    public function responseAccepted($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Sucessfully accepted'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 202);
+    }
+
+    public function responseNotFound($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Not Found'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 404);
+
+    }
+
+    public function responseBadRequest($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Bad Request'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 400);
+    }
+
+    public function responseForbidden($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Forbidden'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 403);
+    }
+
+    public function responseUnauthorized($message = null, $metadata = null){
+        $response = [
+            'success' => true,
+            'message' => $message ?? 'Unauthorized'
+        ];
+        if ($metadata) $response['data'] = $metadata;
+        return response()->json($response, 401);
+    }
 }
